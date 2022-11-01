7e751fd1437d4f6eecf239a531c59825fd49e639
UPRTru <uprt.mi2@yandex.ru>
2022-11-02 01:22:38 +0300

    controllers-films-users

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,60 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/films")
+public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int id = 1;
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        if (films.containsKey(film.getId())) {
+            log.error("Фильм с id: " + film.getId() + " уже существует");
+            throw new ValidationException("Фильм с id: " + film.getId() + " уже существует");
+        }
+        checkDate(film);
+        film.setId(generateId());
+        log.info("addFilm " + film);
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film film) {
+        if (films.containsKey(film.getId())) {
+            log.error("Фильм с id: " + film.getId() + " уже существует");
+            throw new ValidationException("Фильм с id: " + film.getId() + " уже существует");
+        }
+        checkDate(film);
+        film.setId(generateId());
+        log.info("updateFilm " + film);
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    private int generateId() {
+        return id++;
+    }
+
+    @GetMapping
+    public List<Film> getFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    private void checkDate (Film film) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1895, Calendar.DECEMBER, 28);
+        if (film.getReleaseDate().before(calendar.getTime())) {
+            log.error("дата релиза — не раньше 28 декабря 1895 года");
+            throw new ValidationException("дата релиза — не раньше 28 декабря 1895 года");
+        }
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,84 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+    private int id = 1;
+
+    @PostMapping
+    public User newUser(@RequestBody User user) {
+        if (users.containsKey(user.getId())) {
+            log.error("Пользователь с таким id уже существует");
+            throw new ValidationException("Пользователь с таким id уже существует");
+        }
+
+        if (checkUserLogin(user.getLogin())) {
+            log.error("Пользователь с логином: " + user.getLogin() + " уже существует");
+            throw new ValidationException("Пользователь с логином: " + user.getLogin() + " уже существует");
+        }
+
+        user.setId(generateId());
+
+        if (user.getName() == null || user.getName().equals("")) {
+            user.setName(user.getLogin());
+        }
+
+        log.info("newUser " + user);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    private int generateId() {
+        return id++;
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) {
+        if (!users.containsKey(user.getId())) {
+            log.error("Пользователя с id: " + user.getId() + " не существует.");
+            throw new ValidationException("Пользователя с id: " + user.getId() + " не существует.");
+        }
+
+        if (checkUserLogin(user.getLogin())) {
+            log.error("Пользователь с логином: " + user.getLogin() + " уже существует");
+            throw new ValidationException("Пользователь с логином: " + user.getLogin() + " уже существует");
+        }
+
+        if (user.getName() == null || user.getName().equals("")) {
+            user.setName(user.getLogin());
+        }
+
+        log.info("updateUser " + user);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    private boolean checkUserLogin(String login) {
+        boolean result = false;
+        for (User user: users.values()) {
+            if (user.getLogin().equals(login)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return new ArrayList<>(users.values());
+    }
+}
+
